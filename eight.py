0edd443be0cf55e56caeef86b4dbf4dea8768e18
Shaikshavali2004 <shaikshavali1019@gmail.com>
2025-05-22 19:16:47 +0530

    Creation of list with 5 elements

---
+++ eight.py
@@ -0,0 +1,9 @@
+#Creation of a list of 5 elements.
+eids = [101,102,103,104,105]
+print("The list of employee ids is:", eids)
+print(eids[0])
+print(eids[1])
+print(eids[2])
+print(eids[3])
+print(eids[4])
+print(eids[5]) # This will raise an IndexError
